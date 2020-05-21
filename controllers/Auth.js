d56242bbbc3e17e4752870ef96d4dc9e8983200f
Vitali Aleinik <vitali_aleinik@senla.eu>
2020-05-21 14:52:09 +0300

    add hash hook to schema

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -3,7 +3,16 @@ const { ErrorResponse } = require('../helpers/ErrorHandler');
 
 module.exports = {
   register: async (req, res) => {
-    res.status(200).json({
+    const { name, email, password, role } = req.body;
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+      role,
+    });
+
+    return res.status(200).json({
       success: true,
     });
   },
