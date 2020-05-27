962fd6c4911a500a7a37ba1fed07713e64ff9cd8
Mikuhann <ng.kano@gmail.com>
2020-05-27 23:03:06 +0300

    add admins crund functionality

--- controllers/User.js
+++ controllers/User.js
@@ -31,4 +31,60 @@ module.exports = {
       payload: user,
     });
   },
+  getAllUsers: async (req, res) => {
+    res.status(200).json(res.queryResults);
+  },
+  getUser: async (req, res) => {
+    const { id } = req.params;
+
+    const user = await User.findById(id);
+
+    if (!user) {
+      throw new ErrorResponse(ErrorMessage('user', id), 404);
+    }
+
+    return res.status(200).json({
+      success: true,
+      payload: user,
+    });
+  },
+  createUser: async (req, res) => {
+    const user = await User.create(req.body);
+
+    return res.status(201).json({
+      success: true,
+      payload: user,
+    });
+  },
+  updateUser: async (req, res) => {
+    const { id } = req.params;
+
+    const user = await User.findByIdAndUpdate(id, filterFalseValues(req.body), {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!user) {
+      throw new ErrorResponse(ErrorMessage('user', id), 404);
+    }
+
+    return res.status(200).json({
+      success: true,
+      payload: user,
+    });
+  },
+  deleteUser: async (req, res) => {
+    const { id } = req.params;
+
+    const user = await User.findByIdAndDelete(id);
+
+    if (!user) {
+      throw new ErrorResponse(ErrorMessage('user', id), 404);
+    }
+
+    return res.status(200).json({
+      success: true,
+      payload: {},
+    });
+  },
 };
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,26 @@
 const UserRouter = require('express').Router();
 const UserController = require('../controllers/User');
+const User = require('../models/User');
 const { asyncMiddleware } = require('../middlewares/AsyncMiddleware');
-const { protectRoute } = require('../middlewares/Auth');
+const { protectRoute, checkRole } = require('../middlewares/Auth');
+const QueryMiddleware = require('../middlewares/QueryMiddleware');
+const { roles } = require('../constants/Roles');
 
-UserRouter.route('/update').put(
-  protectRoute,
-  asyncMiddleware(UserController.updateProfile)
-);
+UserRouter.use(protectRoute);
+
+UserRouter.route('/update').put(asyncMiddleware(UserController.updateProfile));
+
+UserRouter.route('/')
+  .get(
+    QueryMiddleware(User),
+    checkRole(roles.ADMIN),
+    asyncMiddleware(UserController.getAllUsers)
+  )
+  .post(checkRole(roles.ADMIN), asyncMiddleware(UserController.createUser));
+
+UserRouter.route('/:id')
+  .get(checkRole(roles.ADMIN), asyncMiddleware(UserController.getUser))
+  .put(checkRole(roles.ADMIN), asyncMiddleware(UserController.updateUser))
+  .delete(checkRole(roles.ADMIN), asyncMiddleware(UserController.deleteUser));
 
 module.exports = UserRouter;
